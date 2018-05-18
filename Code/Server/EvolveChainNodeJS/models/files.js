d5717a76ba20857d21c319c36400e58f9ae99db7
Arpit Jain <arpit.jain@newgen.co.in>
2018-05-18 16:40:02 +0530

    New models for documents and files

--- Code/Server/EvolveChainNodeJS/models/files.js
+++ Code/Server/EvolveChainNodeJS/models/files.js
@@ -8,25 +8,28 @@ const Schema = mongoose.Schema;
 const config = require('config');
 
 const FileSchema = new Schema({
-    chunkSize : {type: Number},
-    //file: {type: Schema.Types.ObjectId, ref: "fs.files"},
-    filename: {type: String},
-    length: {type: Number},
-    md5: {type: String},
-    metadata: { filename: {type: String}},
-    uploadDate: {
-        type: Date,
-        default: Date.now
-    },
-    details_profile_img_id: {type: String},
-    details_address_proof_id: {type: String},
-    details_passport1_id: {type: String},
-    details_passport2_id: {type: String},
-    details_identity1_id: {type: String},
-    details_identity2_id: {type: String},
-    details_tax1_id: {type: String},
-    details_drivinglicence1_id: {type: String},
-    details_hold_img_id: {type: String}
+    // chunkSize : {type: Number},
+    // //file: {type: Schema.Types.ObjectId, ref: "fs.files"},
+    // filename: {type: String},
+    // length: {type: Number},
+    // md5: {type: String},
+    // metadata: { filename: {type: String}},
+    // uploadDate: {
+    //     type: Date,
+    //     default: Date.now
+    // },
+    // details_profile_img_id: {type: String},
+    // details_address_proof_id: {type: String},
+    // details_passport1_id: {type: String},
+    // details_passport2_id: {type: String},
+    // details_identity1_id: {type: String},
+    // details_identity2_id: {type: String},
+    // details_tax1_id: {type: String},
+    // details_drivinglicence1_id: {type: String},
+    // details_hold_img_id: {type: String}
+    data: Buffer,
+    contentType: String,
+    key : String
 });
 
 
---
+++ Code/Server/EvolveChainNodeJS/models/kycdocument.js
@@ -0,0 +1,38 @@
+
+/*
+ * Country model schema
+ *
+ */
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const config = require('config');
+
+// const ImageData = new Schema({
+//     img: { data: Buffer, contentType: String }
+// });
+
+const ImageInfo = new Schema({
+    name: String, 
+    contentType: String,
+    encoding : String
+});
+
+const DocInfo = new Schema({
+    details : {type : Schema.Types.Mixed},
+    images : {type: [ImageInfo]},
+    docType :{type: String}
+});
+
+const KYCDocument = new Schema({
+    appId : {type: String},
+    isDelete : {type:Boolean},
+    docInfo : {type : [DocInfo]}
+});
+
+KYCDocument.pre('save', function(next) {
+    var KYCDocument = this;
+    next();
+});
+
+module.exports = mongoose.model('kycdocuments', KYCDocument);
