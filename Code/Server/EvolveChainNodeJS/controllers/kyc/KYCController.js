948e876860335d595045962cf70f7e4e14996c31
RiteekaSapru <riteeka.sapru@newgen.co.in>
2018-06-20 18:24:08 +0530

    error messages added

--- Code/Server/EvolveChainNodeJS/controllers/kyc/KYCController.js
+++ Code/Server/EvolveChainNodeJS/controllers/kyc/KYCController.js
@@ -87,7 +87,7 @@ class KYCController extends BaseController {
                     req.checkBody("number", messages.req_number).notEmpty();
                     break;
                 default:
-                    return this.SendErrorResponse(res, config.ERROR_CODES.ERROR, "Step name missing");
+                    return this.SendErrorResponse(res, config.ERROR_CODES.STEP_NAME_MISSING);
                     break;
             }
 
@@ -122,7 +122,7 @@ class KYCController extends BaseController {
                 case "identity":
                     var idx = proofDocumentCodes.indexOf(body.substep);
                     if(idx==-1)
-                        return this.SendErrorResponse(res, config.ERROR_CODES.ERROR, "Invalid Address Document type!");
+                        return this.SendErrorResponse(res, config.ERROR_CODES.INVALID_ADDRESS_DOCUMENT_TYPE);
                     break;
                 default:
                     break;
@@ -142,7 +142,7 @@ class KYCController extends BaseController {
             if (!app) return this.SendErrorResponse(res, config.ERROR_CODES.APP_NOT_FOUND);
 
             if (!app.phone || !app.email)
-                return this.SendErrorResponse(res, config.ERROR_CODES.ERROR, "Phone or Email not yet verified");
+                return this.SendErrorResponse(res, config.ERROR_CODES.EMAIL_PHONE_NOT_VERIFIED);
 
             let infoType = body.step + "_info";
             this.SaveDocumentImages(req.files, (response) => {
@@ -418,7 +418,7 @@ class KYCController extends BaseController {
             );
             if ((all_image_ids.length !== result_all.length)) return kycController.GetErrorResponse(messages.file_not_found, res);
 
-            //link send through email 
+            //link send through email
             var template = fs.readFileSync(config.EMAIL_TEMPLATES_PATH + '/verifyKycRequest.html', {
                 encoding: 'utf-8'
             });
